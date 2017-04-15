62aa53899ff6ac368340963f7b85c5a50abc48ad
Lurunchik <Lurunchik@gmail.com>
2017-04-15 20:23:32 +0500

    add page number

--- my_simple_survey/templates/my_simple_survey/_generate_joke_html.py
+++ my_simple_survey/templates/my_simple_survey/_generate_joke_html.py
@@ -8,33 +8,32 @@ def chunks(l, n):
 
 
 def get_html(jokes, start_id):
-    jokes_html = '\n\n'.join(
-[
-"""<p><strong>Question: </strong>""" + j['question'] + """</p>
+    return '\n\n'.join(
+        [
+            """<p><strong>Question: </strong>""" + j['question'] + """</p>
 <p><strong>Answer: </strong>""" + j['answer'] + """</p>
 {% formfield player.joke_""" + str(i) + """ with label="" %}
 <br>
-""" for i, j in enumerate(jokes, start=start_id)
-            ])
-
-    html = """{% extends "global/Page.html" %}
-{% load staticfiles otree_tags %}
-
-{% block content %}
-<h3>Please, evaluate the following jokes </h3>
-""" + jokes_html + """
-{% next_button %}
-
-
-{% endblock %}
-"""
-    return html.encode('utf8')
+""" for i, j in enumerate(jokes, start=start_id)])
 
 
 if __name__ == "__main__":
 
-    i = 1
-    for t in chunks(JOKES, JOKES_COUNT_ON_PAGE):
-        with open('Joke{}.html'.format(i), 'wb') as f:
-            f.write(get_html(t, i))
-            i = i + len(t)
+    start_page_num = 1
+    joke_chunks = list(chunks(JOKES, JOKES_COUNT_ON_PAGE))
+    for i, t in enumerate(joke_chunks, start=1):
+        with open('Joke{}.html'.format(start_page_num), 'wb') as f:
+            content_block = "<h3>Page {}/{}. Please, evaluate the following jokes: </h3> " \
+                            "{}".format(i, len(joke_chunks),
+                                        get_html(t, start_page_num))
+            html = """{% extends "global/Page.html" %}
+                {% load staticfiles otree_tags %}
+
+                {% block content %}
+                """ + content_block + """
+                {% next_button %}
+
+                {% endblock %}
+            """
+            f.write(html.encode('utf-8'))
+        start_page_num += len(t)
