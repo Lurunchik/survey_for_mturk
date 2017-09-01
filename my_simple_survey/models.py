7e254b39366e8d474cf1a68e3842117962a65d38
Lurunchik <Lurunchik@gmail.com>
2017-09-01 13:54:47 +0500

    add chart

--- my_simple_survey/models.py
+++ my_simple_survey/models.py
@@ -1,3 +1,5 @@
+import enum
+import operator
 import random
 from collections import defaultdict
 
@@ -37,6 +39,24 @@ class Constants(BaseConstants):
             3: 0.193858845096}
     }
 
+    def __get_jokes_borders(self):
+        max_diff, min_diff = 0, 100
+
+        for i, v in self.jokes_avarage_evaluation.items():
+            m_diff = max(v.values()) - min(v.values())
+            if m_diff > max_diff:
+                max_diff = m_diff
+            min_d = max(v.values()) - sorted(v.values(), reverse=True)[1]
+            if min_d and min_d < min_diff:
+                min_diff = min_d
+        return min_diff, max_diff
+
+    @staticmethod
+    def average_score():
+        return sum(max(Constants.jokes_avarage_evaluation[i].items(),
+                       key=operator.itemgetter(1))[0]
+                   for i, v in Constants.jokes_avarage_evaluation.items())
+
 
 class Subsession(BaseSubsession):
     pass
@@ -44,6 +64,12 @@ class Subsession(BaseSubsession):
 
 class Group(BaseGroup):
     pass
+
+
+class HumorTypes(enum.IntEnum):
+    DID_NOT_GET_IT = 0
+    AVERAGE = 1
+    HIGH_FUNNY = 1
 
 
 class Player(BasePlayer):
@@ -122,14 +148,111 @@ class Player(BasePlayer):
         self.code = random.randint(1, 1000000)
         return self.code
 
+    def user_labels(self):
+        return [self.joke_1, self.joke_2, self.joke_3, self.joke_4,
+                self.joke_5, self.joke_6, self.joke_7]
+
     def __evaluate_user(self):
-        user_labels = [self.joke_1, self.joke_2, self.joke_3, self.joke_4,
-                       self.joke_5, self.joke_6, self.joke_7]
         eval_dict = defaultdict(int)
-        for i, label in enumerate(user_labels, start=1):
+        for i, label in enumerate(self.user_labels(), start=1):
             eval_dict[label] += Constants.jokes_avarage_evaluation[i][label]
 
         return sum(eval_dict.values()) / 7
 
     def result(self):
-        return round(self.__evaluate_user()*100)
+        return round(self.__evaluate_user() * 100)
+
+    def __get_diff_with_avg(self):
+        max_diff = defaultdict(int)
+        diff_jokes = defaultdict(dict)
+
+        for i, label in enumerate(self.user_labels(), start=1):
+            max_bath = max(Constants.jokes_avarage_evaluation[i].items(),
+                           key=operator.itemgetter(1))
+            if max_bath[1] != label:
+                diff = max_bath[1] - Constants.jokes_avarage_evaluation[i][label]
+                if diff > max_diff[label]:
+                    diff_jokes[label] = {
+                        'label': label,
+                        'joke_id': i,
+                        'majority_label': max_bath[0],
+                        'diff': round(diff * 100)}
+                    max_diff[label] = diff
+        return diff_jokes, round(max(max_diff.values()) * 100)
+
+    def _user_type(self):
+        user_score = sum(self.user_labels())
+        diff = user_score - Constants.average_score()
+        if abs(diff) < 5:
+            return HumorTypes.AVERAGE
+        if diff < 0:
+            return HumorTypes.DID_NOT_GET_IT
+        if diff > 0:
+            return HumorTypes.HIGH_FUNNY
+
+    def result_text(self):
+        '''
+        min diff: 1
+        max_diff: 40
+        :return:
+        '''
+        return {
+            HumorTypes.AVERAGE:
+                """Great Job!
+                    Your sense of Humor is:
+
+Just right!
+
+You're friendly and funny, interesting and able to smile genuinely. You tend to give great advice, but you shouldn't get offended too easily.""",
+            HumorTypes.DID_NOT_GET_IT: """
+            Great Job!
+            Your sense of Humor is:
+you don't get it!
+It's ok, you'll get the hang of it, but keep trying. 
+A lot of people may make fun of you, but try to stand up for yourself! 
+Don't let little things get you down, you're sweet, cute, loyal and basically like a fluffy bunny.
+            """,
+            HumorTypes.HIGH_FUNNY: """
+            Great Job!
+            Your sense of Humor is:
+
+Extreme
+You tend to be bouncy, fun and very optimistic, a good friend and a huge laugh! You can always make someone smile, but CALM DOWN! You're too hyper! Sometimes you go a bit too far too impress people or yourself. You don't need humor ALL the time. Profile D"""
+        }.get(self._user_type())
+
+    def has_chart(self):
+        diff, max_diff = self.__get_diff_with_avg()
+        return diff and max_diff > 3
+
+    def chart(self):
+        label_by_pic = {'😞': 0, '😔': 1, '😊': 2, '😆': 3}
+        template = [{
+            'name': 'Marks',
+            'colorByPoint': True,
+            'data': [{
+                'name': '😞',
+                'y': 0,
+            }, {
+                'name': '😔',
+                'y': 0,
+            }, {
+                'name': '😊',
+                'y': 0,
+            }, {
+                'name': '😆',
+                'y': 0
+            }]
+        }]
+        diff, max_diff = self.__get_diff_with_avg()
+
+        joke = next(v for v in diff.values() if v['diff'] == max_diff)
+
+        joke_scores = Constants.jokes_avarage_evaluation[joke['joke_id']]
+        for data in template[0]['data']:
+            data['y'] = joke_scores[label_by_pic[data['name']]]
+            if joke['label'] == label_by_pic[data['name']]:
+                data['sliced'] = True
+                data['selected'] = True
+                data['name'] += ' - Your answer here!!!'
+
+        return template
