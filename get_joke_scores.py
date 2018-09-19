ba6e8a0c52e939c7902c3b8538111c3855b9e962
Vladislav Blinov <cunningplan@yandex.ru>
2018-09-19 21:32:41 +0500

    Debug joke scores

---
+++ get_joke_scores.py
@@ -0,0 +1,23 @@
+import collections
+import os
+
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+django.setup()
+
+from my_simple_survey.models import Joke, JokeScore
+
+
+jokes = Joke.objects.all()
+joke_scores = JokeScore.objects.all()
+
+joke_scores_aggregated = collections.defaultdict(list)
+for score in joke_scores:
+    joke_scores_aggregated[score.joke].append(score.score)
+
+for joke in jokes:
+    if joke.id in joke_scores_aggregated:
+        print(joke.text)
+        print(joke.is_joke)
+        print(joke_scores_aggregated[joke.id])
